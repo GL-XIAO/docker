8249df0e335bc475c4fdd0febfdbe8fd097be9ac
wenlongdeng <xiao_long_deng@163.com>
2021-04-22 18:46:56 +0800

    修改测试接口 内容:邓文龙DSB2222

--- src/main/java/com/example/docker/controller/DockerController.java
+++ src/main/java/com/example/docker/controller/DockerController.java
@@ -8,6 +8,6 @@ public class DockerController {
 
     @RequestMapping("/hello")
     public String add(){
-        return "hello word 邓文龙DSB1111";
+        return "hello word 邓文龙DSB2222";
         }
 }
