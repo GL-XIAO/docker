2c6413c74533a6a846fbcb56deb8a2395c59b8fb
wenlongdeng <xiao_long_deng@163.com>
2021-04-22 14:16:43 +0800

    上传测试文件

---
+++ src/main/java/com/example/docker/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.example.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @RequestMapping("/hello")
+    public String add(){
+        return "hello word";
+        }
+}
